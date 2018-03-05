1b45ac0ba8ea07714dea20eaef1860f38e893b6b
wojjar <jaroslaw@wojcik.hub.pl>
2018-03-05 10:24:28 +0100

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,140 @@
+//============ Przycisk zainicjowania nowej gry
+
+var newGameBtn = document.getElementById('js-newGameButton');
+newGameBtn.addEventListener('click', newGame);
+
+//============ Wybór gracza
+
+var pickRock = document.getElementById('js-playerPick_rock'),
+    pickPaper = document.getElementById('js-playerPick_paper'),
+    pickScissors = document.getElementById('js-playerPick_scissors');
+
+
+pickRock.addEventListener('click', function () { playerPick('rock') });
+pickPaper.addEventListener('click', function () { playerPick('paper') });
+pickScissors.addEventListener('click', function () { playerPick('scissors') });
+
+//============ Wartości początkowe
+
+var gameState = 'notStarted', //started // ended
+    player = {
+        name: '',
+        score: 0
+    },
+    computer = {
+        score: 0
+    };
+//============ Zmienne wskazujące na elemnty gry
+
+var newGameElem = document.getElementById('js-newGameElement'),
+    pickElem = document.getElementById('js-playerPickElement'),
+    resultsElem = document.getElementById('js-resultsTableElement');
+
+//============ Wyświetlanie elementów gry
+
+function setGameElements() {
+    switch (gameState) {
+        case 'started':
+            newGameElem.style.display = 'none';
+            pickElem.style.display = 'block';
+            resultsElem.style.display = 'block';
+            break;
+        case 'ended':
+            newGameBtn.innerText = 'Play again';
+        case 'notStarted':
+        default:
+            newGameElem.style.display = 'block';
+            pickElem.style.display = 'none';
+            resultsElem.style.display = 'none';
+    }
+}
+setGameElements();
+
+//============ Rozpoczęcie gry
+
+var playerPointsElem = document.getElementById('js-playerPoints'),
+    playerNameElem = document.getElementById('js-playerName'),
+    computerPointsElem = document.getElementById('js-computerPoints');
+
+//============ Uruchamiana po wciśnięciu przycisku "NewGame"
+
+function newGame() {
+    player.name = prompt('Please enter your name', 'imię gracza');
+    if (player.name) {
+        player.score = computer.score = 0;
+        gameState = 'started';
+        setGameElements();
+        playerNameElem.innerHTML = player.name;
+        setGamePoints(); // This function has  been created 
+    }
+}
+//============= Pobranie wyboru gracza
+
+function playerPick(playerPick) {
+    console.log(playerPick);
+}
+
+//============= Losowanie wyboru komputera
+
+function getComputerPick() {
+    var possiblePicks = ['rock', 'paper', 'scissors'];
+    return possiblePicks[Math.floor(Math.random() * 3)];
+}
+///============ Umieszczanie wyboru na stronie
+
+var playerPickElem = document.getElementById('js-playerPick'),
+    computerPickElem = document.getElementById('js-computerPick'),
+    playerResultElem = document.getElementById('js-playerResult'),
+    computerResultElem = document.getElementById('js-computerResult');
+
+function playerPick(playerPick) {
+    var computerPick = getComputerPick();
+
+    playerPickElem.innerHTML = playerPick;
+    computerPickElem.innerHTML = computerPick;
+
+    checkRoundWinner(playerPick, computerPick);
+}
+
+function checkRoundWinner(playerPick, computerPick) {
+    playerResultElem.innerHTML = computerResultElem.innerHTML = '';
+    var winnerIs = 'player';
+    if (playerPick == computerPick) {
+        winnerIs = 'noone';
+    } else if (
+        (computerPick == 'rock' && playerPick == 'scissors') ||
+        (computerPick == 'scissors' && playerPick == 'paper') ||
+        (computerPick == 'paper' && playerPick == 'rock')) {
+        winnerIs = 'computer';
+    }
+    if (winnerIs == 'player') {
+        playerResultElem.innerHTML = "Win!";
+        player.score++;
+    } else if (winnerIs == 'computer') {
+        computerResultElem.innerHTML = "Win!";
+        computer.score++;
+    }
+    setGamePoints();
+    finishGame();
+}
+
+//============ Aktualizacja wyniku
+
+function setGamePoints() {
+    playerPointsElem.innerHTML = player.score;
+    computerPointsElem.innerHTML = computer.score;
+}
+
+//============== Gramy do 10
+
+function finishGame() {
+    if (player.score === 10) {
+        alert(player.name + " wins!")
+        gameState = 'ended'
+    }
+    else if (computer.score === 10) {
+        alert("Computer wins!");
+        gameState = 'ended'
+    }
+    setGameElements();
+}   
